2361af208d9d9378df6c8498566471102fba93d3
Faizsheik <faizsheik008@gmail.com>
2026-05-15 07:18:56 +0530

    added vercel json data

--- server/server.js
+++ server/server.js
@@ -2,28 +2,68 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-const toDoRoutes  = require('./ToDoRoutes');
+const toDoRoutes = require('./ToDoRoutes');
 const authRoutes = require('./authroutes');
 
 require('dotenv').config();
 
-const PORT = process.env.PORT||5000;
 app.use(cors());
-//Middleware
 app.use(express.json());
 
-app.use('/api',authRoutes);
-app.use('/api/todo',toDoRoutes);
+app.use('/api', authRoutes);
+app.use('/api/todo', toDoRoutes);
 
-mongoose.connect(process.env.DB_URL).then((result)=>
-{
+mongoose.connect(process.env.DB_URL)
+.then(() => {
     console.log("Database connected");
-}).catch((err)=>
-{
-    console.log("Db not connected successfully",err);
 })
+.catch((err) => {
+    console.log("Db not connected successfully", err);
+});
 
-app.listen(PORT,()=>
-{
-    console.log(`server started at port ${PORT}`);
-})
+app.get("/", (req, res) => {
+   res.send("Backend is running successfully");
+});
+
+module.exports = app;
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+// const toDoRoutes  = require('./ToDoRoutes');
+// const authRoutes = require('./authroutes');
+
+// require('dotenv').config();
+
+// const PORT = process.env.PORT||5000;
+// app.use(cors());
+// //Middleware
+// app.use(express.json());
+
+// app.use('/api',authRoutes);
+// app.use('/api/todo',toDoRoutes);
+
+// mongoose.connect(process.env.DB_URL).then((result)=>
+// {
+//     console.log("Database connected");
+// }).catch((err)=>
+// {
+//     console.log("Db not connected successfully",err);
+// })
+// app.get("/", (req, res) => {
+//    res.send("Backend is running successfully");
+// });
+
+// app.listen(PORT,()=>
+// {
+//     console.log(`server started at port ${PORT}`);
+// })
+
+
+
+
